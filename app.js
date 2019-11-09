1dd7b9bce94e113b15f7f42a6b0286ac813b8c83
An Huynh <anhuynh26@gmail.com>
2019-11-08 22:29:06 -0600

    working type writer text with array of strings

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
 var i = 0;
-// var txt = 'Hi, I\'m Anne.';
+var j = 0;
+var done = false;
+var txt = ['Hello!', 'My name is Anne!', 'Nice to meet you.']
 
-wordIndex = 0;
-var txts = ['hello!', 'bonjour!', 'beeeeetch.']
-
-
-function typeWriter(txt) {
-    var conTxt = txt;
-    if (i < conTxt.length) {
-        document.getElementsByClassName('typewrite')[0].innerHTML += conTxt.charAt(i);
-        i++;
-        setTimeout(typeWriter(conTxt), 65);
+function typeWriter() {
+    if (j < txt.length) {
+        if (i < txt[j].length) {
+            document.getElementsByClassName('typewrite')[0].innerHTML += txt[j].charAt(i);
+            i++;
+            setTimeout(typeWriter, 65);
+        } else {
+            j++;
+            i = 0;
+            setTimeout(function () {
+                document.getElementsByClassName('typewrite')[0].innerHTML = '';
+                typeWriter();
+            }, 2000)
+        }
     }
-    setTimeout(function () {
-        document.getElementsByClassName('typewrite')[0].innerHTML = '';
-    }, 3000);
 }
 
-
-
-// if (wordIndex < txts.length){   
-    setTimeout(typeWriter(txts[0]), 1000);
-
-
-
-//     i = 0;
-//     wordIndex = wordIndex + 1;
-//     typeWriter();
-// }
+typeWriter();
 
 
